2fabe0ab5ab8db50cf8914a86954623fc7f73ba8
ChristinaTsouknida <christina.tsouknida@gmail.com>
2026-07-26 19:20:43 +0300

    fix: change styling

--- src/shared/layout/Layout.tsx
+++ src/shared/layout/Layout.tsx
@@ -2,15 +2,14 @@ import Header from './Header'
 import Footer from './Footer'
 import {Outlet} from "react-router";
 
-
 const Layout = () => {
-
   return (
       <>
-        <div className="flex flex-col h-screen bg-mt-black"
-             style={{backgroundImage: "url('/login-bg.jpg')", backgroundSize: "cover", backgroundPosition: "center"}}>
+        <div className="flex flex-col h-screen bg-mt-black">
           <Header/>
-          <main className="flex-1 overflow-auto container mx-auto flex items-center justify-center"><Outlet/></main>
+          <main className="flex-1 overflow-auto container mx-auto flex items-center justify-center">
+            <Outlet/>
+          </main>
           <Footer/>
         </div>
       </>
