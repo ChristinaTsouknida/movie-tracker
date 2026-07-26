bf3278ca6746ad09dd47c341ae36cbaf657f54a5
ChristinaTsouknida <christina.tsouknida@gmail.com>
2026-07-26 21:55:15 +0300

    feat: add register route

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import HomePage from "./pages/HomePage.tsx";
 import {Routes, Route} from "react-router";
 import MyListPage from "./pages/MyListPage.tsx";
 import LoginPage from "./pages/LoginPage.tsx";
+import RegisterPage from "./pages/RegisterPage.tsx";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
             <Route index element={<LoginPage />} />
             <Route path="home" element={<HomePage />} />
             <Route path="my-list" element={<MyListPage />} />
+            <Route path="register" element={< RegisterPage/>} />
           </Route>
         </Routes>
       </>
