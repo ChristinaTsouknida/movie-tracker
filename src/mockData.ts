ee395946ba58673f11675e90b90b29d7dec1f3da
ChristinaTsouknida <christina.tsouknida@gmail.com>
2026-07-28 20:36:00 +0300

    feat: add more sample movies

--- src/mockData.ts
+++ src/mockData.ts
@@ -3,4 +3,12 @@ import type {Movie} from "./shared/types.ts";
 export const mockMovies: Movie[] = [
     {id: 1, title: "Titanic", year: 1996, category: "Drama", posterUrl: ""},
     {id: 2, title: "Spiderman", year: 2026, category: "Action", posterUrl: ""},
+    {id: 3, title: "Cars", year: 2006, category: "Action", posterUrl: ""},
+    {id: 4, title: "Cars 2", year: 2011, category: "Action", posterUrl: ""},
+    {id: 5, title: "Joker", year: 2019, category: "Crime", posterUrl: ""},
+    {id: 6, title: "Twilight", year: 2008, category: "Fantasy", posterUrl: ""},
+    {id: 7, title: "The Lion King", year: 1994, category: "Adventure", posterUrl: ""},
+    {id: 8, title: "Purple Hearts", year: 2022, category: "Drama", posterUrl: ""},
+    {id: 9, title: "Enola Holmes", year: 2020, category: "Adventure", posterUrl: ""},
+    {id: 10, title: "Enola Holmes 2", year: 2022, category: "Adventure", posterUrl: ""},
 ]
