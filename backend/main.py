8d7bf4d0ed3bb1a72366878b0110f49ec4dcbde0
ChristinaTsouknida <christina.tsouknida@gmail.com>
2026-07-29 20:09:42 +0300

    feat: initial FastAPI setup with movies endpoint

---
+++ backend/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Movie(BaseModel):
+    id: int
+    title: str
+    year: int
+    category: str
+    posterUrl: str
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Movie Tracker API is running"}
+
+
+movie1 = Movie(id=1, title="Titanic", year=1996, category="Drama", posterUrl="empty")
+movie2 = Movie(id=2, title="Batman", year=2022, category="Action", posterUrl="empty")
+
+movies_list = [movie1, movie2]
+
+@app.get("/movies")
+def read_movies():
+    return movies_list
