3c5babfe962287536422dd813a2431c81654fc14
ChristinaTsouknida <christina.tsouknida@gmail.com>
2026-07-30 20:11:09 +0300

    feat: change TMDB schema response

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
 from app.core.database import engine, Base
 from app.models.movie import Movie as MovieModel
 from sqlalchemy.orm import Session
 from app.core.database import get_db
 from app.repositories.movie_repository import get_all_movies
-from typing import List
+from typing import List, Optional
+from app.services.tmdb_service import search_movies
 
 class Movie(BaseModel):
     id: int
@@ -14,6 +15,17 @@ class Movie(BaseModel):
     year: int
     category: str
     posterUrl: str = Field(validation_alias="poster_url")
+
+class TMDBSearchResult(BaseModel):
+    tmdb_id: int = Field(validation_alias="id")
+    title: str
+    year: int = Field(validation_alias="release_date")
+    posterUrl: Optional[str] = Field(validation_alias="poster_path", default=None)
+
+    @field_validator("year", mode="before")
+    @classmethod
+    def extract_year(cls, value):
+        return int(value[:4])
 
 app = FastAPI()
 
@@ -35,3 +47,8 @@ def read_root():
 @app.get("/movies", response_model=List[Movie])
 def read_movies(db: Session = Depends(get_db)):
     return get_all_movies(db)
+
+
+@app.get("/movies/search", response_model=List[TMDBSearchResult])
+def search_movies_endpoint(query: str):
+    return search_movies(query)
