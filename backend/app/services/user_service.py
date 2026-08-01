88630896adf16c777d9da1746c6ad7944a2c5540
ChristinaTsouknida <christina.tsouknida@gmail.com>
2026-08-01 11:19:21 +0300

    feat: add login_user to user_service

--- backend/app/services/user_service.py
+++ backend/app/services/user_service.py
@@ -1,5 +1,5 @@
 from app.repositories.user_repository import get_user_by_email, create_user
-from app.core.security import hash_password
+from app.core.security import hash_password, verify_password
 
 def register_user(db, full_name, email, password):
     existing_user = get_user_by_email(db, email)
@@ -9,3 +9,14 @@ def register_user(db, full_name, email, password):
     hashed_password = hash_password(password)
 
     return create_user(db=db, full_name=full_name, email=email, hashed_password=hashed_password)
+
+
+def login_user(db, email, password):
+    user = get_user_by_email(db, email)
+    if not user:
+        raise ValueError("Invalid email or password")
+
+    if not verify_password(password, user.hashed_password):
+        raise ValueError("Invalid email or password")
+
+    return user
