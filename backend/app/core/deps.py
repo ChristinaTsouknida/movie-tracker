adac1e328500d23f28a7895337c070929efd58d0
ChristinaTsouknida <christina.tsouknida@gmail.com>
2026-08-02 18:49:55 +0300

    fix: change security from FastAPI

--- backend/app/core/deps.py
+++ backend/app/core/deps.py
@@ -1,13 +1,14 @@
 from fastapi import Depends, HTTPException
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from sqlalchemy.orm import Session
 from app.core.database import get_db
 from app.core.security import decode_access_token
 from app.repositories.user_repository import get_user_by_email
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+oauth2_scheme = HTTPBearer()
 
-def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    token = credentials.credentials
     email = decode_access_token(token)
     user = get_user_by_email(db, email)
     if not user:
