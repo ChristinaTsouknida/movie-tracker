ce7efd2c973955e2d2a9a2238d05342ef99299bf
ChristinaTsouknida <christina.tsouknida@gmail.com>
2026-08-02 19:08:26 +0300

    feat: add UserMovie repository with create function

---
+++ backend/app/repositories/user_movie_repository.py
@@ -0,0 +1,8 @@
+from app.models.user_movie import UserMovie
+
+def create_user_movie(db, user_id, movie_id, status):
+    new_movie = UserMovie(user_id=user_id, movie_id=movie_id, status=status)
+    db.add(new_movie)
+    db.commit()
+    db.refresh(new_movie)
+    return new_movie
