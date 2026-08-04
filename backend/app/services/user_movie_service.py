b7a3b09260266670ece9ad0733b3a45786d5533f
ChristinaTsouknida <christina.tsouknida@gmail.com>
2026-08-04 18:38:18 +0300

    feat: add get_my_movies service

--- backend/app/services/user_movie_service.py
+++ backend/app/services/user_movie_service.py
@@ -1,4 +1,8 @@
-from app.repositories.user_movie_repository import create_user_movie
+from app.repositories.user_movie_repository import create_user_movie, get_user_movies
 
 def add_to_list(db, user_id, movie_id, status):
     return create_user_movie(db=db, user_id=user_id, movie_id=movie_id, status=status)
+
+
+def get_my_movies(db, user_id):
+    return get_user_movies(db=db, user_id=user_id)
