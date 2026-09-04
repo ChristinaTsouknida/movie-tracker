c08c65224ec1fc0f25261d0160a2b127ccf8de32
ChristinaTsouknida <christina.tsouknida@gmail.com>
2026-09-04 23:12:22 +0300

    fix: change link to Header

--- frontend/src/shared/layout/Header.tsx
+++ frontend/src/shared/layout/Header.tsx
@@ -14,7 +14,7 @@ const Header = () => {
       <>
         <header className="bg-mt-dark-gray fixed w-full py-4 z-20">
           <div className="container mx-auto px-4 flex items-center justify-between">
-            <Link to="/" className="text-white font-bold text-lg">Movie Tracker</Link>
+            <Link to="/home" className="text-white font-bold text-lg">Movie Tracker</Link>
             <nav className="flex items-center gap-6 text-white font-medium">
               <Link to="/home">Home</Link>
               <Link to="/my-list">My List</Link>
